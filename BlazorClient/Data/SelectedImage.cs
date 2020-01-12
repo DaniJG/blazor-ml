42d9d396b6879ee20040c0c7d834ecbcc9edc641
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2020-01-12 19:04:34 +0100

    cleanup SelectedImage and add upload progress bar

--- BlazorClient/Data/SelectedImage.cs
+++ BlazorClient/Data/SelectedImage.cs
@@ -7,26 +7,32 @@ namespace BlazorClient.Data
 {
     public class SelectedImage
     {
-        public IFileListEntry File { get; set; }
-        public string Base64Image { get; set; }
+        private IFileListEntry _file;
+        public string Base64Image { get; private set; }
         public ImageClassificationResult ClassificationResult { get; set; }
-        public double UploadedPercentage => 100.0 * File.Data.Position / File.Size;
+        public string Name => _file.Name;
+        public double UploadedPercentage => 100.0 * _file.Data.Position / _file.Size;
+
+        public SelectedImage(IFileListEntry file)
+        {
+            _file = file;
+        }
 
         public async Task<MemoryStream> Upload(Action OnDataRead)
         {
-            if (File.Data.Position > 0) throw new InvalidOperationException("Already uploaded");
+            if (_file.Data.Position > 0) throw new InvalidOperationException("Already uploaded");
 
             EventHandler eventHandler = (sender, eventArgs) => OnDataRead();
-            File.OnDataRead += eventHandler;
+            _file.OnDataRead += eventHandler;
 
             // Download File contents into a memory stream, so we can later hand it over to the ML.NET service
             var fileStream = new MemoryStream();
-            await File.Data.CopyToAsync(fileStream);
+            await _file.Data.CopyToAsync(fileStream);
 
             // Get a base64 so we can render an image preview
             Base64Image = Convert.ToBase64String(fileStream.ToArray());
 
-            File.OnDataRead -= eventHandler;
+            _file.OnDataRead -= eventHandler;
             return fileStream;
         }
     }
